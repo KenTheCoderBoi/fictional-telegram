f0e9dfbd0e43e1ef5ab6e602cd466c1b47fd300e
dancingcookieman <74688350+dancingcookieman@users.noreply.github.com>
2021-07-02 16:12:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById("bulb").innerHTML = "<image src='on+bulb.png'></image>"
+    console.log("on")
+}
+function off(){
+    document.getElementById("bulb").innerHTML = "<image src='off+bulb.png'></image>"
+}
